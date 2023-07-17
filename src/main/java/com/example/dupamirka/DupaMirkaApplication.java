4327d72f0c747b52d88dab4c2404dbed82b980b1
Marcel <marcelrozanski1988@gmail.com>
2023-07-17 09:22:11 +0200

    MF-1 incjalny comit aplikacji

---
+++ src/main/java/com/example/dupamirka/DupaMirkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.dupamirka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DupaMirkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DupaMirkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dupamirka/DupaMirkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dupamirka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DupaMirkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
